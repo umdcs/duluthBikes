d9edea1e0ddcbd0d1b8155fb59af7644be644c6d
Sam <mart3315@d.umn.edu>
2017-03-15 20:23:26 -0500

    Added buttons and map fragments, updated app colors

--- app/src/main/java/com/example/sam/duluthbikes/MainActivity.java
+++ app/src/main/java/com/example/sam/duluthbikes/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sam.duluthbikes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -9,7 +10,7 @@ import android.view.View;
  * Created by Sam on 3/13/2017.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
 
     @Override
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     public void displayMap(View view){
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
@@ -25,3 +27,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
