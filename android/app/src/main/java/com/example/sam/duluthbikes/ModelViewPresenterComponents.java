84fe2f6a9ad4d71f20c722bb184c7beeb842d637
Sam <mart3315@d.umn.edu>
2017-03-18 11:59:34 -0500

    Added interface classes to ModelViewPresenterComponents interface

--- android/app/src/main/java/com/example/sam/duluthbikes/ModelViewPresenterComponents.java
+++ android/app/src/main/java/com/example/sam/duluthbikes/ModelViewPresenterComponents.java
@@ -1,9 +1,23 @@
 package com.example.sam.duluthbikes;
 
+import android.location.Location;
+
 /**
  * Created by Sam on 3/18/2017.
  */
 
 public interface ModelViewPresenterComponents {
 
+    interface View {
+        void notifyModelUpdated();
+    }
+
+    interface Presenter {
+        ///
+    }
+
+    interface Model {
+        String getLocation();
+        void setLocation(Location curr);
+    }
 }
