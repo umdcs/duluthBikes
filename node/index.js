6f2c8ed4ce71557f7f825376d4674134833902b3
Kevin Bowditch <bowdi006@d.umn.edu>
2017-03-23 14:17:41 -0500

    added so users can get put, pulled, and deleted from server

--- node/index.js
+++ node/index.js
@@ -87,4 +87,98 @@ app.get('/getRouteData', function(request,response) {
 
 app.listen(app.get("port"), function () {
 	console.log('duluth bikes node listening on port: ', app.get("port"));
-});
+})
+
+
+.get('/users', function(req, res) {
+	
+		if (!req.body) return res.sendStatus(400)
+	
+		var jsonArray = [];
+		
+		for(var key in users) {
+					jsonObject = {"username": key};
+			        jsonArray.push(jsonObject);
+				}
+		console.log(JSON.stringify(jsonArray));
+		res.send(JSON.stringify(jsonArray));
+})
+
+
+//Get protocol for all user objects
+app.get('/users', function(req, res) {
+
+	if (!req.body) return res.sendStatus(400)
+	var jsonArray = [];
+	
+	for(var key in users) {
+		jsonObject = {"username": key};
+        jsonArray.push(jsonObject);
+	}
+	console.log(JSON.stringify(jsonArray));
+	res.send(JSON.stringify(jsonArray));
+})
+
+
+// POST protocol for new user
+	app.post('/users', function (req, res) {
+		//If the JSON isn't parsed, return an error
+		if (!req.body) return res.sendStatus(400)
+
+
+	// Variables for user profile
+		var quotesUser = JSON.stringify(req.body.Username);
+		console.log("user to add = " + quotesUser);
+
+		for(var key in users) {
+			console.log("existing user: " + key);
+			var quotesKey = "\"" + key + "\"";
+			console.log("quotesKey = " + quotesKey);
+			if (quotesKey == quotesUser) {
+				console.log("Duplicate username");
+				return res.sendStatus(500)
+		}
+	}
+		
+
+		var newUser = {
+			"friends": [],
+			"rides": []
+		}
+			console.log("userName = " + quotesUser);
+			users[req.body.Username] = newUser;
+
+			res.send(req.body);
+	})
+
+
+// DELETE protocol for user
+	 app.delete('/users', function (req, res) {
+	// If the JSON isn't parsed, return an error
+ 	    if (!req.body) return res.sendStatus(400)
+
+	    	var userName = req.body.Username;
+
+	    		delete users[userName];
+
+	    			return res.send(req.body);
+	    			})
+
+//Get protocol for user profile objects
+	app.get('/profile/:user', function(req, res) {
+	var name = req.params.user;
+	var profileObj;
+	var found = false;
+
+	for(var key in users){
+		if(key == name){
+			profileObj = JSON.stringify(users[key]);
+			found = true;
+												}
+											}
+
+											if(found)
+												return res.send(profileObj)
+											else
+																						return res.sendStatus(400)
+																					})
