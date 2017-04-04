d2422407e44b471df99935d96f982a121cfb154a
Ruta Wheelock <wheel477@d.umn.edu>
2017-04-04 17:45:06 -0500

    Enabled Back key when browsing webview.

--- android/app/src/main/java/com/example/sam/duluthbikes/EventsFragment.java
+++ android/app/src/main/java/com/example/sam/duluthbikes/EventsFragment.java
@@ -1,8 +1,10 @@
 package com.example.sam.duluthbikes;
 
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,25 +20,51 @@ import android.webkit.WebViewClient;
 public class EventsFragment extends Fragment {
 
     View myView;
+    WebView myWebView;
     String EventsPage = "http://www.duluthbikes.org/news-events/";
-
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.activity_events, container, false);
 
-        WebView myWebView = (WebView) myView.findViewById(R.id.webViewEvents);
+        myWebView = (WebView) myView.findViewById(R.id.webViewEvents);
         myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.setWebViewClient(new newWebClient());
         myWebView.loadUrl(EventsPage);
+        myWebView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if(keyEvent.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    WebView webView = (WebView) view;
+
+                    switch(keyCode)
+                    {
+                        case KeyEvent.KEYCODE_BACK:
+                            if(webView.canGoBack())
+                            {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
+                    }
+                }
+
+                return false;
+            }
+        });
+
 
         return myView;
     }
+
+
 
     private class newWebClient extends WebViewClient {
 
     }
 
-}
 
+
+}
