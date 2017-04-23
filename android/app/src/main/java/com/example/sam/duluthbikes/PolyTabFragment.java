35d424d227002fba131a76318176250a81406cbb
Sam <mart3315@d.umn.edu>
2017-04-23 06:57:35 -0500

    Added polylines map to ridehistory tab

--- android/app/src/main/java/com/example/sam/duluthbikes/PolyTabFragment.java
+++ android/app/src/main/java/com/example/sam/duluthbikes/PolyTabFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 /**
  * Created by Sam on 4/20/2017.
@@ -15,13 +17,26 @@ import android.webkit.WebView;
 public class PolyTabFragment extends Fragment {
 
     View myView;
-    WebView myWebView = null;
+    WebView myWebView;
     String PolylinePage = "http://ukko.d.umn.edu:23405/rides/";
+    ProgressBar pb;
 
+    @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.activity_poly_tab, container, false);
+        myWebView = (WebView) myView.findViewById(R.id.webViewPolylineTab);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        pb = (ProgressBar) myView.findViewById(R.id.progressBar);
+        pb.setVisibility(View.VISIBLE);
+
+        myWebView.setWebViewClient(new newWebClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                pb.setVisibility(View.INVISIBLE);
+            }
+        });
+        myWebView.loadUrl(PolylinePage);
         return myView;
     }
+    private class newWebClient extends WebViewClient { }
 }
-
-
